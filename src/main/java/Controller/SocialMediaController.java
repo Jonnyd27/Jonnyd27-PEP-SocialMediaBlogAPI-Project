2d4cd41e0eef287b68bd8865ae160f6067980411
Jonnyd27 <Jonnyd27@github.com>
2026-04-16 16:51:23 +0000

    Test run: 0/23 passed, 23 failed, 0 skipped - 2026-04-16T16:51:23.871Z

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -3,6 +3,14 @@ package Controller;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import DAO.AccountDAO;
+import Service.AccountService;
+import Model.Account;
+import Model.Message;
+
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
@@ -14,10 +22,17 @@ public class SocialMediaController {
      * suite must receive a Javalin object from this method.
      * @return a Javalin app object which defines the behavior of the Javalin controller.
      */
+
+
+     AccountService accountService;
+     public SocialMediaController(){
+        accountService = new AccountService();
+     }
+
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
-
+        app.get("register",this::postAccountHandler);
         return app;
     }
 
@@ -27,6 +42,17 @@ public class SocialMediaController {
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+    private void postAccountHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.addAccount(account);
+        if(addedAccount==null) {
+            ctx.status(400);
+        }else{
+            ctx.json(mapper.writeValueAsString(addedAccount));
+        }
     }
 
 
