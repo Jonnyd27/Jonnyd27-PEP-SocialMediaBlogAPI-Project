b9b276133fae8f1455aefe5478f58bfca2ec0163
Jonnyd27 <Jonnyd27@github.com>
2026-04-17 16:51:23 +0000

    Test run: 14/23 passed, 9 failed, 0 skipped - 2026-04-17T16:51:23.384Z

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -75,7 +75,7 @@ public class MessageDAO {
 
         ResultSet rs = ps.executeQuery();
         while(rs.next()) {
-          return new Message(message_id, rs.getInt("posted_by"), rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+          return new Message(rs.getInt("message_id"), rs.getInt("posted_by"), rs.getString("message_text"), rs.getLong("time_posted_epoch"));
         }
       }catch(SQLException e) {
         System.out.println(e.getMessage());
