0e6dd506644a539bd7a8f485b2d4bc5651564a65
Jonnyd27 <Jonnyd27@github.com>
2026-04-17 16:05:24 +0000

    Test run: 7/23 passed, 16 failed, 0 skipped - 2026-04-17T16:05:24.472Z

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -29,17 +29,17 @@ public class MessageDAO {
     public Message insertMessage(Message message) {
       Connection connection = ConnectionUtil.getConnection();
       try{
-        String sql = "insert into message (posted_by, message_text, time_posted_epoch) values (?,?,?)";
+        String sql = "insert into message (posted_by, message_text, time_posted_epoch) values (?,?,?);";
         PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        ps.setInt(1, message.posted_by);
-        ps.setString(2, message.message_text);
-        ps.setLong(3,message.time_posted_epoch);
+        ps.setInt(1, message.getPosted_by());
+        ps.setString(2, message.getMessage_text());
+        ps.setLong(3,message.getTime_posted_epoch());
         ps.executeUpdate();
         ResultSet pkeyResultSet = ps.getGeneratedKeys();
 
         if(pkeyResultSet.next()) {
           int generated_message_id = (int) pkeyResultSet.getLong(1);
-          return new Message(generated_message_id, message.posted_by, message.message_text, message.time_posted_epoch);
+          return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
         }
       }catch(SQLException e) {
         System.out.println(e.getMessage());
